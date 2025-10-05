238216c4ffca3e54e7157e1f1e2a3305533a30ec
Happiness Mhlongo <mhlongohappiness880@gmail.com>
2025-10-05 20:22:00 +0200

    Fixed my NavBar to have favorite link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,35 @@
 
+import React from "react";
+import { Link } from "react-router-dom"; 
 
+function NavBar() {
+  return (
+    <div>
+      <nav className="flex justify-between items-center p-4 bg-gray-100 shadow">
+        
+        <Link to="/" className="font-bold text-lg">
+          Recipe Finder
+        </Link>
 
-
-function NavBar(){
-    return(
-        <div>
-         <nav className="flex justify-between items-center p-4 bg-gray-100 shadow">
-          <Link to="/" className="font-bold text-lg">Recipe Finder</Link>
-         <div className="flex gap-4">
+        
+        <div className="flex gap-4">
           <Link
             to="/favorites"
-             className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-           >
-             My Favorites
-           </Link>
-       </div>
-        </nav>
+            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          >
+            My Favorites
+          </Link>
         </div>
-    )
+      </nav>
+    </div>
+  );
 }
 
 export default NavBar;
+
+
+
+
+
+
+
